1cbfc630189355917d1b76409b808c84c55b506e
wlsdn <0503wlsdn@gmail.com>
2015-08-18 20:07:28 +0900

    Alarm color change
    알람 진행중에 색상 변경

--- app/src/main/java/com/soma/albert/jachwibot/Robot_Alarm.java
+++ app/src/main/java/com/soma/albert/jachwibot/Robot_Alarm.java
@@ -2,6 +2,7 @@ package com.soma.albert.jachwibot;
 
 import org.roboid.robot.Device;
 import org.roboid.robot.Robot;
+import java.util.Random;
 
 import kr.robomation.physical.Albert;
 
@@ -12,11 +13,10 @@ public class Robot_Alarm implements Runnable {
     private Thread main_thread;
     private CallBackEvent callBackEvent;
 
-    private Device RightProximity;
-    private Device LeftProximity;
+    private Device RightProximity, LeftProximity;
+    private Device RightWheel, LeftWheel;
+    private Device RightEye, LeftEye;
     private Device Acceleration;
-    private Device RightWheel;
-    private Device LeftWheel;
 
     private boolean Alram_Start_Flag = false;
 
@@ -30,6 +30,8 @@ public class Robot_Alarm implements Runnable {
         Acceleration = robot.findDeviceById(Albert.SENSOR_ACCELERATION);
         RightWheel = robot.findDeviceById(Albert.EFFECTOR_RIGHT_WHEEL);
         LeftWheel = robot.findDeviceById(Albert.EFFECTOR_LEFT_WHEEL);
+        RightEye = robot.findDeviceById(Albert.EFFECTOR_RIGHT_EYE);
+        LeftEye = robot.findDeviceById(Albert.EFFECTOR_LEFT_EYE);
     }
 
     public void setCallBackEvent(CallBackEvent callback) {
@@ -51,46 +53,70 @@ public class Robot_Alarm implements Runnable {
 
     @Override
     public void run() {
-        boolean obstacle_flag = false;
-        while (Alram_Start_Flag) {
-            try {
-                Thread.sleep(200); //200ms delay
+        Random random = new Random();
+        boolean obstacle_flag = false; //장애물을 피하는 중인지 여부
 
-                int accelerationZ = Acceleration.read(2); // Z축 가속도 값을 읽는다.
-                int leftProximity = LeftProximity.read();
-                int rightProximity = RightProximity.read();
-                if ((leftProximity > 50) || (rightProximity > 50) || obstacle_flag) {
-                    if ((leftProximity > 35) && (rightProximity > 35)) {
-                        obstacle_flag = true;
-                        if (leftProximity > rightProximity) {
-                            LeftWheel.write(100);
-                            RightWheel.write(-100);
-                        } else {
-                            LeftWheel.write(-100);
-                            RightWheel.write(100);
-                        }
+        boolean increment_flag = true;
+        int leftEye_value[] = {LeftEye.read(0),  LeftEye.read(1), LeftEye.read(2)}; //왼쪽눈의 RGB값
+        int rightEye_value[] = {RightEye.read(0),  RightEye.read(1), RightEye.read(2)}; //오른쪽눈의 RGB값
+
+        while (Alram_Start_Flag) try {
+            Thread.sleep(200); //200ms delay
+
+            int accelerationZ = Acceleration.read(2); // Z축 가속도 값을 읽는다.
+            int leftProximity = LeftProximity.read(); //왼쪽 거리센서
+            int rightProximity = RightProximity.read(); //오른쪽 거리센서
+            int random1 = random.nextInt(3);
+            int random2 = random.nextInt(3);
+
+            //전진 및 장애물 회피
+            if ((leftProximity > 50) || (rightProximity > 50) || obstacle_flag) {
+                if ((leftProximity > 35) && (rightProximity > 35)) {
+                    obstacle_flag = true;
+                    if (leftProximity > rightProximity) {
+                        LeftWheel.write(100);
+                        RightWheel.write(-100);
                     } else {
-                        obstacle_flag = false;
+                        LeftWheel.write(-100);
+                        RightWheel.write(100);
                     }
                 } else {
-                    LeftWheel.write(100);
-                    RightWheel.write(100);
+                    obstacle_flag = false;
                 }
-
-                //넘어짐 감지
-                if (accelerationZ > 0) {
-                    Alram_Start_Flag = false;
-                    LeftWheel.write(0);
-                    RightWheel.write(0);
-                    if (callBackEvent != null) {
-                        callBackEvent.Robot_Alram_Stop();
-                    }
-                }
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } else {
+                LeftWheel.write(100);
+                RightWheel.write(100);
             }
+
+            //LED색 변경
+            if (increment_flag) {
+                leftEye_value[random1] += 50;
+                rightEye_value[random2] += 50;
+            } else {
+                leftEye_value[random1] -= 50;
+                rightEye_value[random2] -= 50;
+            }
+            if ((leftEye_value[random1] > 250) || (rightEye_value[random2] > 250) ||
+                    (leftEye_value[random1] == 0) || (rightEye_value[random2] == 0)) {
+                increment_flag ^= true;
+            }
+            LeftEye.write(leftEye_value);
+            RightEye.write(rightEye_value);
+
+            //넘어짐 감지
+            if (accelerationZ > 0) {
+                LeftWheel.write(0);
+                RightWheel.write(0);
+                LeftEye.write(new int[]{0, 0, 0});
+                RightEye.write(new int[]{0, 0, 0});
+                set_Alram_status(false);
+                if (callBackEvent != null) {
+                    callBackEvent.Robot_Alram_Stop();
+                }
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
-
 }
