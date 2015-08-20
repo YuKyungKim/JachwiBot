59f35a650b22aea2f8d7857073a3b304677472c9
wlsdn <0503wlsdn@gmail.com>
2015-08-20 20:30:20 +0900

    Update

--- app/src/main/java/com/soma/albert/jachwibot/MainActivity.java
+++ app/src/main/java/com/soma/albert/jachwibot/MainActivity.java
@@ -3,6 +3,7 @@ package com.soma.albert.jachwibot;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,12 +14,43 @@ import org.smartrobot.android.RobotActivity;
 
 public class MainActivity extends RobotActivity implements View.OnClickListener{
 
+    private static final int ALARM_REQUEST = 10;
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.albertConnectBtn) {
-            Intent serviceIntent = new Intent(this, LuncherService.class);
-            startService(serviceIntent);
+        switch (v.getId())
+        {
+            case R.id.albertConnectBtn:
+                Intent serviceIntent = new Intent(this, LuncherService.class);
+                startService(serviceIntent);
+                break;
+            case R.id.Alarm_set:
+                Intent intent = new Intent(this, AlarmActivity.class);
+                intent.putExtra("Class", this.getClass());
+                startActivityForResult(intent, ALARM_REQUEST);
+                break;
+            case R.id.Alarm_Cancel:
+                AlarmActivity.alarm_cancel(this, AlarmActivity.MORNING_CALL);
+                AlarmActivity.alarm_cancel(this, 1);
+                break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case ALARM_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    int Alarm_data = data.getIntExtra("Alarm_ID", -1);
+                    Log.d("Alarm", "ID = " + String.valueOf(Alarm_data));
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.d("Alarm", "ID = " + String.valueOf(intent.getIntExtra("ID", -1)) +
+                " Repeat = " + String.valueOf(intent.getBooleanExtra("Repeat", false)));
     }
 
     @Override
@@ -27,27 +59,7 @@ public class MainActivity extends RobotActivity implements View.OnClickListener{
         setContentView(R.layout.activity_main);
         Button albertConnectBtn = (Button) findViewById(R.id.albertConnectBtn);
         albertConnectBtn.setOnClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        findViewById(R.id.Alarm_set).setOnClickListener(this);
+        findViewById(R.id.Alarm_Cancel).setOnClickListener(this);
     }
 }
