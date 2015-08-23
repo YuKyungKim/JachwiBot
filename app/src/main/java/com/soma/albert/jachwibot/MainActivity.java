c4aeea505962ddd4ab38ab4f7b13c3e58b014db1
YuKyungKim <whee6409@gmail.com>
2015-08-23 15:57:03 +0900

    housework and db connect

--- app/src/main/java/com/soma/albert/jachwibot/MainActivity.java
+++ app/src/main/java/com/soma/albert/jachwibot/MainActivity.java
@@ -1,7 +1,9 @@
 package com.soma.albert.jachwibot;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
@@ -32,12 +35,14 @@ import org.roboid.robot.Device;
 import org.roboid.robot.Robot;
 import org.smartrobot.android.RobotActivity;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import kr.robomation.physical.Albert;
-
 
 public class MainActivity extends RobotActivity implements View.OnClickListener {
 
@@ -61,25 +66,27 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
     // TODO 예보 따로 따오기..
 
     // 집안일 패널과 알람 패널
-    GridView houseworkGridView, alarmGridView;
+    GridView  houseworkGridView, alarmGridView;
     private ArrayList<HouseworkComponent> houseCompList = new ArrayList();
     private ArrayList<AlarmComponent> alarmCompList = new ArrayList();
+    private AlertDialog dialog = null;
 
     // bottom panel - conversation with albert
     private Conversation conversation;
     private Device mSpeakerDevice;
-    public String simsimi_response="";
+    public String simsimi_response = "";
 
     // gps
     private double lat;
     private double lon;
 
     private static final int ALARM_REQUEST = 10;
+
     @Override
-    public void onInitialized(Robot robot)
-    {
+    public void onInitialized(Robot robot) {
         mSpeakerDevice = robot.findDeviceById(Albert.EFFECTOR_SPEAKER);
     }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -105,13 +112,16 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
                 }
                 check();
                 break;
+
             // TODO 집안일 및 알람 추가 버튼 만들기
 
         }
     }
-    public void check(){
+
+    public void check() {
 
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
@@ -124,23 +134,11 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
         }
     }
 
-    protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
-        super.onCreate(savedInstanceState);
-
-        //get Context to Decoder
-        Decoder_pcm decoder = new Decoder_pcm(this);
-        //get Context to Decoder
-        conversation= new Conversation(this);
-
-        currentTempText = (TextView) findViewById(R.id.curtemptext);
-        maxMinTempText = (TextView) findViewById(R.id.maxMinTempText);
-        weatherIcon = (ImageView) findViewById(R.id.weatherImageIcon);
-        weatherText = (TextView) findViewById(R.id.weatherText);
-        isRain = (ImageView) findViewById(R.id.unbrellaIcon);
-
+    @Override
+    protected void onResume() {
+        super.onResume();
         // db
-        final DBManager dbManager = new DBManager(getApplicationContext(),"jachwibot.db", null, 1);
+        final DBManager dbManager = new DBManager(getApplicationContext(), "jachwibot.db", null, 1);
 
         // gps lat, lon data communicate
         gps = new GpsInfo(MainActivity.this);
@@ -155,23 +153,95 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
 
         // housework panel
         houseworkGridView = (GridView) findViewById(R.id.houseworkGridView);
-        houseworkGridView.setAdapter(new houseworkGridAdapter());
+        final HouseworkGridAdapter houseworkGridAdapter = new HouseworkGridAdapter();
+        houseworkGridView.setAdapter(houseworkGridAdapter);
         // db data call - housework
-        ArrayList<HouseworkComponent> houseworkList = dbManager.selectAllHoseworkData();
-        for(int i = 0; i < houseworkList.size(); i++) {
-            houseCompList.add(houseworkList.get(i));
+        final ArrayList<HouseworkComponent> houseworkList = dbManager.selectAllHoseworkData();
+        houseCompList.clear();
+        if(houseworkList != null) {
+            for (int i = 0; i < houseworkList.size(); i++) {
+                houseCompList.add(houseworkList.get(i));
+            }
         }
-        // TODO 추가 버튼 만들것.
+        // 추가 버튼
+        HouseworkComponent plus = new HouseworkComponent(0, 0, null);
+        houseCompList.add(plus);
+
+        houseworkGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (parent.getLastVisiblePosition() == position) {
+                    Intent intent = new Intent(view.getContext(), HouseworkActivity.class);
+                    startActivityForResult(intent, 0);
+                } else {
+                    dialog = createInflaterDialog(dbManager, houseCompList.get(position).getHouseworkId(), position);
+                    dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            //Log.i("dismiss", "call");
+                            houseworkGridAdapter.notifyDataSetChanged();
+                        }
+                    });
+                    dialog.show();
+
+                }
+            }
+        });
+
 
         // alarm panel
         alarmGridView = (GridView) findViewById(R.id.alarmGridView);
         alarmGridView.setAdapter(new alarmGridAdapter());
         // db data call - alarm
         ArrayList<AlarmComponent> alarmList = dbManager.selectAllAlarmData();
-        for(int i = 0; i < alarmList.size(); i++) {
-            alarmCompList.add(alarmList.get(i));
+        if(alarmList != null) {
+            for (int i = 0; i < alarmList.size(); i++) {
+                alarmCompList.add(alarmList.get(i));
+            }
         }
+
         // TODO 추가 버튼 만들것.
+
+    }
+
+    private AlertDialog createInflaterDialog(final DBManager dbManager, final int houseCmpId, final int position) {
+        final View innerView = getLayoutInflater().inflate(R.layout.housework_dialog, null);
+        AlertDialog.Builder ab = new AlertDialog.Builder(this);
+        ab.setTitle("삭제 확인");
+        ab.setView(innerView);
+        ab.setPositiveButton("삭제", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dbManager.delete("DELETE FROM HOUSEWORK_LIST WHERE housework_id = " + houseCmpId + ";");
+                houseCompList.remove(position);
+            }
+        });
+
+        ab.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        return ab.create();
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+
+        setTitle("자취봇");
+
+        //get Context to Decoder
+        Decoder_pcm decoder = new Decoder_pcm(this);
+        //get Context to Decoder
+        conversation = new Conversation(this);
+
+        currentTempText = (TextView) findViewById(R.id.curtemptext);
+        maxMinTempText = (TextView) findViewById(R.id.maxMinTempText);
+        weatherIcon = (ImageView) findViewById(R.id.weatherImageIcon);
+        weatherText = (TextView) findViewById(R.id.weatherText);
+        isRain = (ImageView) findViewById(R.id.unbrellaIcon);
 
         // bottom panel
         // albert connect button
@@ -185,10 +255,10 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
         findViewById(R.id.Alarm_Cancel).setOnClickListener(this);
     }
 
-    public class houseworkGridAdapter extends BaseAdapter {
+    public class HouseworkGridAdapter extends BaseAdapter {
         LayoutInflater inflater;
 
-        public houseworkGridAdapter() {
+        public HouseworkGridAdapter() {
             inflater = (LayoutInflater) act.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
@@ -210,23 +280,55 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
-                convertView = inflater.inflate(R.layout.alarm_component, parent, false);
+                convertView = inflater.inflate(R.layout.housework_component, parent, false);
             }
 
-            ImageView cmpIcon = (ImageView) convertView.findViewById(R.id.cmpIcon);
-            TextView cmpText = (TextView) convertView.findViewById(R.id.cmpText);
+            ImageView cmpIcon = (ImageView) convertView.findViewById(R.id.houseCmpIcon);
+            TextView cmpText = (TextView) convertView.findViewById(R.id.houseCmpText);
 
             HouseworkComponent cmp = houseCompList.get(position);
 
-            if (cmp.getHouseworkId() == 1) {
+            if (cmp.getHouseworkType() == 1) {
                 cmpIcon.setImageResource(R.drawable.trash);
-                cmpText.setText(cmp.getHouseworkText());
-            } else if (cmp.getHouseworkId() == 2) {
+                cmpText.setText(calcDay(cmp.getLastDay()));
+            } else if (cmp.getHouseworkType() == 2) {
                 cmpIcon.setImageResource(R.drawable.washing_machine);
-                cmpText.setText(cmp.getHouseworkText());
+                cmpText.setText(calcDay(cmp.getLastDay()));
+            } else if (cmp.getHouseworkType() == 0) {
+                cmpIcon.setImageResource(R.drawable.plus);
+                cmpText.setText("추가");
             }
             return convertView;
         }
+    }
+
+    public String calcDay(String lastDate) {
+        Calendar todayCal = Calendar.getInstance();
+        Calendar lastdayCal = Calendar.getInstance();
+
+        StringTokenizer strTk = new StringTokenizer(lastDate, ".");
+        int lastYear = Integer.parseInt(strTk.nextToken());
+        int lastMonth = Integer.parseInt(strTk.nextToken());
+        int lastDay = Integer.parseInt(strTk.nextToken());
+
+        lastdayCal.set(lastYear, lastMonth - 1, lastDay);
+
+        long delta = todayCal.getTimeInMillis() - lastdayCal.getTimeInMillis() ;
+
+        lastdayCal.setTimeInMillis(delta);
+
+        int date = lastdayCal.get(Calendar.DAY_OF_YEAR) + 1;
+
+        long gap = (delta / 86400000);
+
+        if (gap == 0) {
+            return "오늘";
+        } else if (gap == 1) {
+            return "어제";
+        } else {
+            return "" + gap  + "일 전";
+        }
+
     }
 
     public class alarmGridAdapter extends BaseAdapter {
@@ -338,43 +440,43 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
     public void weather() {
         msgHandler = new Handler() {
             public void dispatchMessage(Message msg) {
-            String tmax = "0", tmin = "0", tcurent = "0", precipType = null;
-            String skyName = null, skyCode = "";
-            try {
-                JSONParser jsonParser = new JSONParser();
-                JSONObject data = (JSONObject) jsonParser.parse(hndResult);
-                JSONObject weather = (JSONObject) data.get("weather");
-                //Log.i("weather", ""+weather);
-                JSONArray minutely = (JSONArray) weather.get("minutely");
-                //Log.i("minutely", ""+minutely);
-                JSONObject minutely1 = (JSONObject) minutely.get(0);
-                //Log.i("minutely1", ""+minutely);
-                // 강수정보
-                // 0: 현상없음, 1: 비, 2: 비/눈, 3: 눈
-                JSONObject precipitation = (JSONObject) minutely1.get("precipitation");
-                precipType = (String) precipitation.get("type");
-                if(precipType.equals("1") || precipType.equals("2") || precipType.equals("3")) {
-                    isRain.setImageResource(R.drawable.umbrella);
+                String tmax = "0", tmin = "0", tcurent = "0", precipType = null;
+                String skyName = null, skyCode = "";
+                try {
+                    JSONParser jsonParser = new JSONParser();
+                    JSONObject data = (JSONObject) jsonParser.parse(hndResult);
+                    JSONObject weather = (JSONObject) data.get("weather");
+                    //Log.i("weather", ""+weather);
+                    JSONArray minutely = (JSONArray) weather.get("minutely");
+                    //Log.i("minutely", ""+minutely);
+                    JSONObject minutely1 = (JSONObject) minutely.get(0);
+                    //Log.i("minutely1", ""+minutely);
+                    // 강수정보
+                    // 0: 현상없음, 1: 비, 2: 비/눈, 3: 눈
+                    JSONObject precipitation = (JSONObject) minutely1.get("precipitation");
+                    precipType = (String) precipitation.get("type");
+                    if (precipType.equals("1") || precipType.equals("2") || precipType.equals("3")) {
+                        isRain.setImageResource(R.drawable.umbrella);
+                    }
+                    // 기온정보
+                    JSONObject temperature = (JSONObject) minutely1.get("temperature");
+                    //Log.i("temperature", ""+temperature);
+                    tmax = (String) temperature.get("tmax");
+                    tmin = (String) temperature.get("tmin");
+                    tcurent = (String) temperature.get("tc");
+                    // 하늘상태정보
+                    JSONObject sky = (JSONObject) minutely1.get("sky");
+                    //Log.i("sky", ""+sky);
+                    skyName = (String) sky.get("name");
+                    skyCode = (String) sky.get("code");
+                } catch (ParseException e) {
+                    e.printStackTrace();
                 }
-                // 기온정보
-                JSONObject temperature = (JSONObject) minutely1.get("temperature");
-                //Log.i("temperature", ""+temperature);
-                tmax = (String)temperature.get("tmax");
-                tmin = (String)temperature.get("tmin");
-                tcurent = (String)temperature.get("tc");
-                // 하늘상태정보
-                JSONObject sky = (JSONObject) minutely1.get("sky");
-                //Log.i("sky", ""+sky);
-                skyName = (String)sky.get("name");
-                skyCode = (String)sky.get("code");
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
 
-            currentTempText.setText(""+tcurent+"℃");
-            maxMinTempText.setText("최고" + tmax.substring(0, 2) + "℃/최저" + tmin.substring(0, 2) + "℃");
-            weatherText.setText(""+skyName);
-            skyIconMatch(skyCode);
+                currentTempText.setText("" + tcurent + "℃");
+                maxMinTempText.setText("최고" + tmax.substring(0, 2) + "℃/최저" + tmin.substring(0, 2) + "℃");
+                weatherText.setText("" + skyName);
+                skyIconMatch(skyCode);
             }
 
             ;
@@ -382,49 +484,49 @@ public class MainActivity extends RobotActivity implements View.OnClickListener 
     }
 
     public void skyIconMatch(String skyCode) {
-        if(skyCode.equals("SKY_A00")) {
+        if (skyCode.equals("SKY_A00")) {
             // 상태없음
             weatherIcon.setImageResource(R.drawable.sky_a00);
-        } else if(skyCode.equals("SKY_A01")) {
+        } else if (skyCode.equals("SKY_A01")) {
             // 맑음
             weatherIcon.setImageResource(R.drawable.sky_a01);
-        } else if(skyCode.equals("SKY_A02")) {
+        } else if (skyCode.equals("SKY_A02")) {
             // 구름조금
             weatherIcon.setImageResource(R.drawable.sky_a02);
-        } else if(skyCode.equals("SKY_A03")) {
+        } else if (skyCode.equals("SKY_A03")) {
             // 구름많음
             weatherIcon.setImageResource(R.drawable.sky_a03);
-        } else if(skyCode.equals("SKY_A04")) {
+        } else if (skyCode.equals("SKY_A04")) {
             // 구름많고 비
             weatherIcon.setImageResource(R.drawable.sky_a04);
-        } else if(skyCode.equals("SKY_A05")) {
+        } else if (skyCode.equals("SKY_A05")) {
             // 구름많고 눈
             weatherIcon.setImageResource(R.drawable.sky_a05);
-        } else if(skyCode.equals("SKY_A06")) {
+        } else if (skyCode.equals("SKY_A06")) {
             // 구름많고 비 또는 눈
             weatherIcon.setImageResource(R.drawable.sky_a06);
-        } else if(skyCode.equals("SKY_A07")) {
+        } else if (skyCode.equals("SKY_A07")) {
             // 흐림
             weatherIcon.setImageResource(R.drawable.sky_a07);
-        } else if(skyCode.equals("SKY_A08")) {
+        } else if (skyCode.equals("SKY_A08")) {
             // 흐리고 비
             weatherIcon.setImageResource(R.drawable.sky_a08);
-        } else if(skyCode.equals("SKY_A09")) {
+        } else if (skyCode.equals("SKY_A09")) {
             // 흐리고 눈
             weatherIcon.setImageResource(R.drawable.sky_a09);
-        } else if(skyCode.equals("SKY_A10")) {
+        } else if (skyCode.equals("SKY_A10")) {
             // 흐리고 비 또는 눈
             weatherIcon.setImageResource(R.drawable.sky_a10);
-        } else if(skyCode.equals("SKY_A11")) {
+        } else if (skyCode.equals("SKY_A11")) {
             // 흐리고 낙뢰
             weatherIcon.setImageResource(R.drawable.sky_a11);
-        } else if(skyCode.equals("SKY_A12")) {
+        } else if (skyCode.equals("SKY_A12")) {
             // 뇌우, 비
             weatherIcon.setImageResource(R.drawable.sky_a12);
-        } else if(skyCode.equals("SKY_A13")) {
+        } else if (skyCode.equals("SKY_A13")) {
             // 뇌우, 눈
             weatherIcon.setImageResource(R.drawable.sky_a13);
-        } else if(skyCode.equals("SKY_A14")) {
+        } else if (skyCode.equals("SKY_A14")) {
             // 뇌우, 비 또는 눈
             weatherIcon.setImageResource(R.drawable.sky_a14);
         } else {
